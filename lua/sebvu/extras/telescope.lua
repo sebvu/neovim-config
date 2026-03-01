7b72e30b028bd4727756f7f45776f8539e39efd5
Jester <jester.sebastiansantos@gmail.com>
2026-03-01 03:46:58 +0000

    Remove fake dependencies

--- lua/sebvu/extras/telescope.lua
+++ lua/sebvu/extras/telescope.lua
@@ -8,7 +8,7 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep", "sharkdp/fd" },
+		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {
 			defaults = {
 				file_ignore_patterns = { "node_modules", ".git", "lazy%-lock.json", "pack" },

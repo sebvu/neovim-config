77cfdafb7d3fa22bb05aab1d6152ba0937012da9
Jester <jester.sebastiansantos@gmail.com>
2026-04-05 05:34:03 -0500

    Add kulala

---
+++ lua/sebvu/plugins/kulala.lua
@@ -0,0 +1,14 @@
+return { -- you can literally make HTTP requests from neovim
+	"mistweaverco/kulala.nvim",
+	keys = {
+		{ "<leader>Rs", desc = "Send request" },
+		{ "<leader>Ra", desc = "Send all requests" },
+		{ "<leader>Rb", desc = "Open scratchpad" },
+	},
+	ft = { "http", "rest" },
+	opts = {
+		global_keymaps = false,
+		global_keymaps_prefix = "<leader>R",
+		kulala_keymaps_prefix = "",
+	},
+}

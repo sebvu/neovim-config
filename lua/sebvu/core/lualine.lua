0ae96619c0cb971b3765d071a15b8b4ea4b7a204
Jester <jester.sebastiansantos@gmail.com>
2026-04-05 15:09:00 -0500

    Update lualine theme

--- lua/sebvu/core/lualine.lua
+++ lua/sebvu/core/lualine.lua
@@ -1,22 +1,17 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		options = {
 			globalstatus = true,
+			theme = "iceberg_dark",
 		},
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff", "diagnostics" },
 			lualine_x = {
-				"encoding",
-				"fileformat",
 				"filetype",
-				{
-					require("noice").api.statusline.mode.get,
-					cond = require("noice").api.statusline.mode.has,
-					-- color = { fg = "#967bb6" },
-				},
+				"fileformat",
+				"encoding",
 			},
 			lualine_y = { "progress" },
 			lualine_z = { "location" },

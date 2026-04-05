0ff8a41b149d787a1543939bc79380f084f94911
Jester <jester.sebastiansantos@gmail.com>
2026-04-05 06:28:50 -0500

    Add documentation for blink.cmp

--- lua/sebvu/core/lsp/blink-cmp.lua
+++ lua/sebvu/core/lsp/blink-cmp.lua
@@ -27,6 +27,10 @@ return { -- actual autocompletion engine
 				},
 			},
 			completion = {
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 500,
+				},
 				menu = {
 					draw = {
 						components = {
